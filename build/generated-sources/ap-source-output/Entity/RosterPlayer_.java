b0b22efd1c18d0582f6ee21b103371a26dd5aac6
samiamorwas <samiamorwas@gmail.com>
2012-11-28 01:03:18 -0500

    Update view of League and My Leagues pages.

--- build/generated-sources/ap-source-output/Entity/RosterPlayer_.java
+++ build/generated-sources/ap-source-output/Entity/RosterPlayer_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2012-11-27T21:37:32")
+@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2012-11-28T01:02:00")
 @StaticMetamodel(RosterPlayer.class)
 public class RosterPlayer_ { 
 
