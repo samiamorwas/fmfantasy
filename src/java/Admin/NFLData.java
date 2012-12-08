dc3b5d410c522aa28d4e4720e8cc95cca135d5fe
Adrian Sears <greasypizza@gmail.com>
2012-12-08 16:49:49 -0500

    Stats stuff

--- src/java/Admin/NFLData.java
+++ src/java/Admin/NFLData.java
@@ -5,134 +5,148 @@
 package Admin;
 
 import Entity.NFLPlayer;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.ejb.Singleton;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
+
+import org.primefaces.json.JSONObject;
+import org.primefaces.json.JSONArray;
+import org.primefaces.json.JSONException;
+
 
 /**
  *
  * @author Greasy
  */
 public class NFLData {
+    static final String urlPrefix = "http://test.nfldata.com/api.svc/JSON/";
+    static final String apiKey = "?key=c7fb0f65-ccb4-4965-bbb9-4cecd0890e10";
     
+    private JSONObject getJSONObjFromURL(String urlString) throws JSONException{
+        try {
+            URL url = new URL(urlString);
+            java.util.Scanner s = new java.util.Scanner(url.openConnection().getInputStream()).useDelimiter("\\A");
+            String str = s.next();
+            return new JSONObject(str);
+        } catch (IOException ex) {
+            Logger.getLogger(NFLData.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    private JSONArray getJSONArrFromURL(String urlString) throws JSONException{
+        try {
+            URL url = new URL(urlString);
+            java.util.Scanner s = new java.util.Scanner(url.openConnection().getInputStream()).useDelimiter("\\A");
+            String str = s.next();
+            return new JSONArray(str);
+        } catch (IOException ex) {
+            Logger.getLogger(NFLData.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    public int getSeasonPointsForPlayer(NFLPlayer nflp) throws JSONException{
+        int points;
+        
+        long nfldataID = nflp.getNFLPlayerID();
+        //if not defense
+        if(nflp.getPosition() != 6){
+            JSONObject jObj = getJSONObjFromURL(urlPrefix + "PlayerSeasonStatsByPlayerID/2011/" + nfldataID + apiKey);
+            points = jObj.getInt("FantasyPoints");
+        }else{
+            JSONArray jArr = getJSONArrFromURL(urlPrefix + "FantasyDefenseBySeason/2011" + apiKey);
+            String teamAbrv = nflp.getTeam();
+            points = -1;
+            for(int i = 0; i < jArr.length(); i++){
+                JSONObject jObj = jArr.getJSONObject(i);
+                if(jObj.getString("Team").equals(teamAbrv)){
+                    points = jObj.getInt("FantasyPoints");
+                    break;
+                }
+            }
+        }
+        return points;
+    }        
+    
+    public int getWeekPointsForPlayer(NFLPlayer nflp, int week){
+        int points;
+        
+        long nfldataID = nflp.getNFLPlayerID();
+        //if not defense
+        try {
+            if(nflp.getPosition() != 6){
+                JSONObject jObj = getJSONObjFromURL(urlPrefix + "PlayerGameStatsByPlayerID/2011/" + week + "/" + nfldataID + apiKey);
+                points = jObj.getInt("FantasyPoints");
+            }else{
+                JSONArray jArr = getJSONArrFromURL(urlPrefix + "FantasyDefenseByGame/2011/" + week + apiKey);
+                String teamAbrv = nflp.getTeam();
+                points = -1;
+                for(int i = 0; i < jArr.length(); i++){
+                    JSONObject jObj = jArr.getJSONObject(i);
+                    if(jObj.getString("Team").equals(teamAbrv)){
+                        points = jObj.getInt("FantasyPoints");
+                        break;
+                    }
+                }
+            }
+        }
+        catch(JSONException ex){
+            Logger.getLogger(NFLData.class.getName()).log(Level.SEVERE, null, ex);
+            points = 0;
+        }
+        return points;        
+    }
     
     public List<NFLPlayer> getAllPlayers(){
         List<NFLPlayer> result = new ArrayList<NFLPlayer>();
-        
-        Document teamsDoc = httpGet("http://test.nfldata.com/api.svc/xml/Teams/2011?key=c7fb0f65-ccb4-4965-bbb9-4cecd0890e10");
-        Element teamsRoot = teamsDoc.getDocumentElement();
-        NodeList teams = teamsRoot.getElementsByTagName("Team");
-        for(int i = 0; i < teams.getLength(); i++){
-            Element team = (Element) teams.item(i);
-            Element teamKey = (Element) team.getElementsByTagName("Key").item(0);
-            Element teamFullName = (Element) team.getElementsByTagName("FullName").item(0);
-            
-            String key = teamKey.getFirstChild().getNodeValue(); //atl, nyj, etc
-            String fullName = teamFullName.getFirstChild().getNodeValue();
-            
-            
-            //get all players
-            String playersReq = "http://test.nfldata.com/api.svc/xml/Players/" + key + "?key=c7fb0f65-ccb4-4965-bbb9-4cecd0890e10";
-            Document playersDoc = httpGet(playersReq);
-            Element playersRoot = playersDoc.getDocumentElement();
-            NodeList players = playersRoot.getElementsByTagName("Player");
-            for(int j = 0; j < players.getLength(); j++){
-                Element player = (Element) players.item(j);
-                Element e_name = (Element) player.getElementsByTagName("Name").item(0);
-                Element e_nflDataID = (Element) player.getElementsByTagName("PlayerID").item(0);
-                Element e_position = (Element) player.getElementsByTagName("Position").item(0);
-                Element e_posCat = (Element) player.getElementsByTagName("PositionCategory").item(0);
-                
-                String s_Name = e_name.getFirstChild().getNodeValue();
-                String s_nflDataID = e_nflDataID.getFirstChild().getNodeValue();
-                String s_position = e_position.getFirstChild().getNodeValue();
-                String s_posCat = e_posCat.getFirstChild().getNodeValue();
-                
-                if(s_posCat.equalsIgnoreCase("DEF")) {
-                    continue;
-                }
-                
-                long l_nflDataID = Long.parseLong(s_nflDataID);
-                
-                int i_position = 0;
-                if(s_position.equals("QB")){
-                    i_position = 1;
-                }
-                if(s_position.equals("RB")){
-                    i_position = 2;
-                }
-                if(s_position.equals("WR")){
-                    i_position = 3;
-                }
-                if(s_position.equals("TE")){
-                    i_position = 4;
-                }
-                if(s_position.equals("K")){
-                    i_position = 5;
-                }
-                
-                NFLPlayer nflp = new NFLPlayer();
-                nflp.setName(s_Name);
-                nflp.setNFLDataID(l_nflDataID);
-                nflp.setPosition(i_position);
-                nflp.setTeam(fullName);
-                
-                result.add(nflp);
-            }
-            
-            //make defense
-            NFLPlayer def = new NFLPlayer();
-            def.setName(fullName);
-            def.setNFLDataID(0);
-            def.setPosition(6);
-            def.setTeam(fullName);
-            
-            result.add(def);
-        }
-        
-        
-        
-        
-        return result;
-    }
-    
-    public Document httpGet(String urlStr) {
-        Document dom = null;
         try {
-            URL url = new URL(urlStr);
-            HttpURLConnection conn =
-                (HttpURLConnection) url.openConnection();
-
-            if (conn.getResponseCode() != 200) {
-              throw new IOException(conn.getResponseMessage());
+            JSONArray teamArr = getJSONArrFromURL(urlPrefix + "Teams/2011" + apiKey);
+            for(int i = 0 ; i < teamArr.length(); i++){
+                JSONObject teamObj = teamArr.getJSONObject(i);
+                String teamFullName = teamObj.getString("FullName");
+                String teamAbrv = teamObj.getString("Key"); 
+                
+                JSONArray pArr = getJSONArrFromURL(urlPrefix + "Players/" + teamAbrv + apiKey);
+                for(int j = 0; j < pArr.length(); j++){
+                    JSONObject pObj = pArr.getJSONObject(j);
+                    if(! pObj.getString("PositionCategory").equals("DEF") ){
+                        String playerName = pObj.getString("Name");
+                        String position = pObj.getString("Position");
+                        int NFLDataID = pObj.getInt("PlayerID");
+                        
+                        NFLPlayer nflp = new NFLPlayer();
+                        nflp.setTeam(teamAbrv);
+                        nflp.setName(playerName);
+                        nflp.setNFLDataID(NFLDataID);
+                        if(position.equals("QB")){
+                            nflp.setPosition(1);
+                        }if(position.equals("RB")){
+                            nflp.setPosition(2);
+                        }if(position.equals("WR")){
+                            nflp.setPosition(3);
+                        }if(position.equals("TE")){
+                            nflp.setPosition(4);
+                        }if(position.equals("K")){
+                            nflp.setPosition(5);
+                        }
+                        result.add(nflp);
+                    }
+                }
+                
+                NFLPlayer def = new NFLPlayer();
+                def.setName(teamFullName);
+                def.setTeam(teamAbrv);
+                def.setPosition(6);
+                def.setNFLDataID(-1);
+                result.add(def);
             }
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            DocumentBuilder db = dbf.newDocumentBuilder();
-            dom = db.parse(conn.getInputStream());
-
             
-        } catch (IOException ex) {
-            Logger.getLogger(NFLData.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SAXException ex) {
-            Logger.getLogger(NFLData.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ParserConfigurationException ex) {
+            
+        } catch (JSONException ex) {
             Logger.getLogger(NFLData.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return dom;
+        return result;
     }
 }
