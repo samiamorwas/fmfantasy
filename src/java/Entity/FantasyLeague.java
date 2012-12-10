eca2cb6a797ae0326dbefe51842fdb7b8165b645
ashay <ashaygandhi90@gmail.com>
2012-12-10 15:48:45 -0500

    Add playoffs, untested.

--- src/java/Entity/FantasyLeague.java
+++ src/java/Entity/FantasyLeague.java
@@ -100,12 +100,6 @@ public class FantasyLeague implements Serializable {
     public void setRosterPlayers(List<RosterPlayer> rosterPlayers) {
         this.rosterPlayers = rosterPlayers;
     }
-     
-    
-    
-    
-    
-    
     @Override
     public int hashCode() {
         int hash = 0;
